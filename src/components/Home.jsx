bf010c90d1eef3c638194421c3c77ea03fb8c2f8
Tomas <tomascorvalan14@gmail.com>
2024-08-25 18:27:00 -0400

    se pone console.log para la pizza 1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,24 @@
+import { useState, useEffect } from "react";
 import NavBar from "./Navbar";
 
 const Home = () => {
+
+
+const [pizzas, setPizzas] = useState([]);
+
+const getPizzas = async () => {
+  const res = await fetch('http://localhost:5000/api/pizzas/p001')
+  const data = await res.json()
+
+  setPizzas(data)
+  console.log(data);
+}
+useEffect(() => {
+getPizzas();
+
+},[])
+
+
   return (
     <div className="FullNavbar">
       <NavBar></NavBar>
